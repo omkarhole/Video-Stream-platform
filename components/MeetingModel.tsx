ea3662a3f02bfdb94be3a7fcd3d307ea810c2eae
omkar hole <omkarhole314@gmail.com>
2025-09-08 23:06:14 +0530

    added modal or dialog box for create meeting with reusable component for schedule meeting

--- components/MeetingModel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MeetingModel = () => {
-  return (
-    <div>MeetingModel</div>
-  )
-}
-
-export default MeetingModel
--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -4,10 +4,15 @@
 import React, { useState } from 'react'
 import HomeCard from '../HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from '../MeetingModal'
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoinMeeting' | 'isInstantMeeting' | undefined>();
+    const createMeeting=()=>{
+
+    }
+    
     return (
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2  xl:grid-cols-4'>
             <HomeCard
@@ -37,6 +42,14 @@ const MeetingTypeList = () => {
                 description='Meeting recordings'
                 handleClick={() => router.push('/recordings')}
                 className='bg-yellow-1'
+            />
+            <MeetingModal 
+            isOpen={meetingState==='isInstantMeeting'}
+                 onClose={()=> setMeetingState(undefined)}
+                 title='Start Instant Meeting'
+                 className='text-center'
+                 buttonText="Start Meeting"
+                 handleClick={createMeeting}
             />
 
         </section>
