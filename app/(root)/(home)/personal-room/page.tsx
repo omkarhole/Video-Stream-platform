32ab98be6ed4193cb347db29c73d8de889cd5692
omkar hole <omkarhole314@gmail.com>
2025-09-11 17:52:08 +0530

    fixed meeting share link url

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -29,7 +29,7 @@ const PersonalRoom = () => {
 
   const { user } = useUser();
   const meetingId = user?.id;
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}/personal=true`;
+  const meetingLink = `https://${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
   const { call } = useGetCallById(meetingId!);
   const client= useStreamVideoClient();
   const router=useRouter();
