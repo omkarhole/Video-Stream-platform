fe00a59d5bfc8e1976989dfa99b972c2e8242055
Rounak <rounakmohata0186@gmail.com>
2026-02-21 19:00:29 +0530

    Issue fixes.

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -63,9 +63,9 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     return (
         <div className='grid grid-cols-1 gap-5 xl:grid-cols-2 2xl:grid-cols-3'>
             {calls && calls.length > 0 ? (
-                calls.map((meeting: Call | CallRecording) => (
+                calls.map((meeting: Call | CallRecording, index) => (
                     <MeetingCard
-                        key={(meeting as Call).id}
+                        key={(meeting as Call).id || index}
                         icon={
                             type === 'ended'
                                 ? '/icons/previous.svg'
